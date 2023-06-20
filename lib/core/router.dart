33051553ad05cc82f8ba8e768585c437bb4f463f
Walter Banda <walterkaimurima@gmail.com>
2023-06-20 15:05:38 +0300

    ☕ Home screen

--- lib/core/router.dart
+++ lib/core/router.dart
@@ -1,3 +1,4 @@
+import 'package:expense_manager/screens/home.dart';
 import 'package:flutter/material.dart';
 
 class Navigation {
@@ -5,8 +6,14 @@ class Navigation {
 
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
+      case Routes.home:
+        return PageRouteBuilder(pageBuilder: (_, __, ___) => Home());
       default:
         return PageRouteBuilder(pageBuilder: (_, __, ___) => Placeholder());
     }
   }
+}
+
+class Routes {
+  static const String home = Home.id;
 }
---
+++ lib/screens/home.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  static const String id = 'home';
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
