f3e2e68aceb253e09db36c1b5e3fddd4f4aa56d6
Walter Banda <walterkaimurima@gmail.com>
2023-06-22 17:14:53 +0300

    ☕ Page transition

--- lib/router/index.dart
+++ lib/router/index.dart
@@ -8,21 +8,54 @@ class Navigation {
   static GlobalKey<NavigatorState> router = GlobalKey();
 
   static Route<dynamic> generateRoute(RouteSettings settings) {
+    var transition = (_, animation, __, child) {
+      const begin = Offset(1.0, 0.0);
+      const end = Offset.zero;
+      const curve = Curves.ease;
+
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+      return SlideTransition(
+        position: animation.drive(tween),
+        child: child,
+      );
+    };
+
     switch (settings.name) {
       case Routes.home:
         return PageRouteBuilder(
-            settings: settings, pageBuilder: (_, __, ___) => Home());
+          settings: settings,
+          pageBuilder: (_, __, ___) => Home(),
+          transitionsBuilder: transition,
+          transitionDuration: Duration(milliseconds: 250),
+        );
       case Routes.settings:
-        return PageRouteBuilder(pageBuilder: (_, __, ___) => Settings());
+        return PageRouteBuilder(
+          pageBuilder: (_, __, ___) => Settings(),
+          transitionsBuilder: transition,
+          transitionDuration: Duration(milliseconds: 250),
+        );
       case Routes.insights:
         return PageRouteBuilder(
-            settings: settings, pageBuilder: (_, __, ___) => Insights());
+          settings: settings,
+          pageBuilder: (_, __, ___) => Insights(),
+          transitionsBuilder: transition,
+          transitionDuration: Duration(milliseconds: 250),
+        );
       case Routes.add:
         return PageRouteBuilder(
-            settings: settings, pageBuilder: (_, __, ___) => AddExpense());
+          settings: settings,
+          pageBuilder: (_, __, ___) => AddExpense(),
+          transitionsBuilder: transition,
+          transitionDuration: Duration(milliseconds: 250),
+        );
       default:
         return PageRouteBuilder(
-            settings: settings, pageBuilder: (_, __, ___) => Placeholder());
+          settings: settings,
+          pageBuilder: (_, __, ___) => Placeholder(),
+          transitionsBuilder: transition,
+          transitionDuration: Duration(milliseconds: 250),
+        );
     }
   }
 
