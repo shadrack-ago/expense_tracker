de05927f49b1abca158ed3ac0ac9a1eb8e398249
Walter Banda <walterkaimurima@gmail.com>
2023-06-27 12:30:40 +0300

    ☕ Sample expense data

--- lib/core/provider/manager.dart
+++ lib/core/provider/manager.dart
@@ -1,3 +1,4 @@
+import 'package:expense_manager/core/models/category.dart';
 import 'package:flutter/widgets.dart' hide MetaData;
 
 import '../models/expense.dart';
@@ -12,7 +13,16 @@ class DataManager extends ChangeNotifier {
     )
   ];
 
+  List<ExpenseCategory> _categories = [
+    ExpenseCategory(
+      meta: MetaData(id: 'dsjfjksdf', timeRecorded: DateTime.timestamp()),
+      name: 'Food',
+      budget: 400,
+    )
+  ];
+
   List<Expense> get expense => _expenses;
+  List<ExpenseCategory> get categories => _categories;
 
   addExpense(Expense expense) {
     _expenses.add(expense);
