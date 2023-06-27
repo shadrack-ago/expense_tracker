670d69de5b02dd3b25d5db4939ff09e57bd83579
Walter Banda <walterkaimurima@gmail.com>
2023-06-27 13:40:49 +0300

    ☕ Using google form for adding expenses

--- lib/screens/add/expense.dart
+++ lib/screens/add/expense.dart
@@ -1,3 +1,4 @@
+import 'package:easy_web_view/easy_web_view.dart';
 import 'package:flutter/material.dart';
 
 class AddExpense extends StatelessWidget {
@@ -5,10 +6,16 @@ class AddExpense extends StatelessWidget {
 
   static const String id = 'add_expense';
 
+  static ValueKey webViewKey = const ValueKey('key_insight');
+
+  final String src =
+      'https://docs.google.com/spreadsheets/d/e/2PACX-1vSGbFtRPwKfGW2rxaWOo8d6zONVIaSTYDbrTRboCNIffzq6bm4bFNof5Rax5Z3QQWepAwZ4tbslEQLY/pubhtml';
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(id),
+    return EasyWebView(
+      key: webViewKey,
+      src: src,
     );
   }
 }
