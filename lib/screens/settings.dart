25aeaac69a87b1d9b27a593123c1042dc8040f91
Walter Banda <walterkaimurima@gmail.com>
2023-06-30 13:09:45 +0300

    ☕ Added form validator, required indicator,update url callback

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -24,24 +24,41 @@ class Settings extends StatelessWidget {
           SizedBox(height: 20),
           TextFormField(
             initialValue: syncManager.sheet,
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Url is required';
+              }
+              return null;
+            },
             decoration: InputDecoration(
               suffixIcon: Icon(Icons.dataset_linked_rounded),
               filled: true,
-              label: Text('Sheet Url'),
+              label: Text('Sheet Url *'),
             ),
           ),
           SizedBox(height: 10),
           TextFormField(
             initialValue: syncManager.docs,
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Url is required';
+              }
+              return null;
+            },
             decoration: InputDecoration(
               suffixIcon: Icon(Icons.document_scanner_rounded),
               filled: true,
-              label: Text('Docs Url'),
+              label: Text('Docs Url *'),
             ),
           ),
           SizedBox(height: 20),
           ElevatedButton(
-            onPressed: () {},
+            onPressed: () {
+              if (_configFormKey.currentState!.validate()) {
+                Provider.of<SyncManager>(context, listen: false)
+                    .updateUrl(sheet: '', docs: '');
+              }
+            },
             child: Text('Save Config'),
             style: ElevatedButton.styleFrom(
               padding: const EdgeInsets.symmetric(vertical: 17, horizontal: 60),
