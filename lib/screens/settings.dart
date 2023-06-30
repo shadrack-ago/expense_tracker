16ebcd7fe2ad3ba48278e17197a2b163c95cb770
Walter Banda <walterkaimurima@gmail.com>
2023-06-30 12:19:26 +0300

    ☕ Styling and customization of settings

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -13,10 +13,24 @@ class Settings extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text('Configure google forms and sheets sources'),
-          TextFormField(),
+          Text(
+            'Configure google forms and sheets sources',
+            style: Theme.of(context).textTheme.titleLarge,
+          ),
+          SizedBox(height: 20),
+          TextFormField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Sheet url',
+            ),
+          ),
           SizedBox(height: 10),
-          TextFormField(),
+          TextFormField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Docs url',
+            ),
+          ),
           SizedBox(height: 10),
           ElevatedButton(onPressed: () {}, child: Text('Save Config'))
         ],
