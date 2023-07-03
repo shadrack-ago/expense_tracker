d87020875743adcef4c949da0af69e19f5b258b7
Walter Banda <walterkaimurima@gmail.com>
2023-07-03 12:30:12 +0300

    ☕ Switching to pure dart implementation

--- lib/screens/add/expense.dart
+++ lib/screens/add/expense.dart
@@ -1,7 +1,4 @@
-import 'package:easy_web_view/easy_web_view.dart';
-import 'package:expense_manager/core/provider/sync.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class AddExpense extends StatelessWidget {
   const AddExpense({super.key});
@@ -12,11 +9,6 @@ class AddExpense extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<SyncManager>(
-      builder: (_, manager, __) => EasyWebView(
-        key: webViewKey,
-        src: manager.docs,
-      ),
-    );
+    return Column();
   }
 }
