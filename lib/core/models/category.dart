39b3063af48c3ecc721017352519cec7997e57df
Walter Banda <walterkaimurima@gmail.com>
2023-07-05 10:54:33 +0300

    ☕ Category validation

--- lib/core/models/category.dart
+++ lib/core/models/category.dart
@@ -11,3 +11,19 @@ class ExpenseCategory {
     required this.budget,
   });
 }
+
+
+class CategoryValidator {
+  static String? validateName(String? name) {
+    if (name == null) return 'An Expense should have a name';
+    return null;
+  }
+
+  static String? validateBudget(String? budget) {
+    if (budget == null)
+      return 'Category should have a budget';
+    else if (budget is int == false)
+      return 'Category budget should be a number';
+    return null;
+  }
+}
