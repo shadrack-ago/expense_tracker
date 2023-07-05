c8407d5d4921e228c6e50dd2c65f53f547773306
Walter Banda <walterkaimurima@gmail.com>
2023-07-05 10:27:33 +0300

    ☕ Serializers for the various types of images

--- lib/core/models/expense.dart
+++ lib/core/models/expense.dart
@@ -35,4 +35,16 @@ class ReceiptImage<T> {
   RImageType type;
   T src;
   ReceiptImage({required this.type, required this.src});
+
+  static ReceiptImage<NetworkImage> fromUrl(String url) {
+    return ReceiptImage(type: RImageType.network, src: NetworkImage(url));
+  }
+
+  static ReceiptImage<FileImage> fromFile(File file) {
+    return ReceiptImage(type: RImageType.file, src: FileImage(file));
+  }
+
+  static ReceiptImage<MemoryImage> fromMemory(Uint8List bytes) {
+    return ReceiptImage(type: RImageType.memory, src: MemoryImage(bytes));
+  }
 }
