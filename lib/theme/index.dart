4049a70e9e1df878c1adc3df79159b01e5d17d8c
Walter Banda <walterkaimurima@gmail.com>
2023-07-08 08:24:25 +0300

    ☕ System navigation bar and status bar transparent

--- lib/theme/index.dart
+++ lib/theme/index.dart
@@ -3,21 +3,24 @@ import 'package:flutter/services.dart';
 
 class Theming {
   static ThemeData theme(BuildContext context) => ThemeData(
-    useMaterial3: true,
-    colorSchemeSeed: Colors.amber,
-    bottomNavigationBarTheme: BottomNavigationBarThemeData(
-      showUnselectedLabels: false,
-      elevation: 0,
-    ),
-    navigationRailTheme: NavigationRailThemeData(
-      labelType: NavigationRailLabelType.selected,
-    ),
-    appBarTheme: AppBarTheme(
-      elevation: 0,
-      systemOverlayStyle: SystemUiOverlayStyle.light,
-    ),
-    listTileTheme: ListTileThemeData(
-      leadingAndTrailingTextStyle: Theme.of(context).textTheme.titleLarge, 
-    ),
-  );
+        useMaterial3: true,
+        colorSchemeSeed: Colors.amber,
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          showUnselectedLabels: false,
+          elevation: 0,
+        ),
+        navigationRailTheme: NavigationRailThemeData(
+          labelType: NavigationRailLabelType.selected,
+        ),
+        appBarTheme: AppBarTheme(
+          elevation: 0,
+          systemOverlayStyle: SystemUiOverlayStyle.dark.copyWith(
+            statusBarColor: Colors.transparent,
+            systemNavigationBarColor: Colors.transparent,
+          ),
+        ),
+        listTileTheme: ListTileThemeData(
+          leadingAndTrailingTextStyle: Theme.of(context).textTheme.titleLarge,
+        ),
+      );
 }
