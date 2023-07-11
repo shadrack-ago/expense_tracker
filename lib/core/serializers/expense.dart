0757bf225e36fb94fc13ebac76e4576c99f9af20
Walter Banda <walterkaimurima@gmail.com>
2023-07-11 11:16:18 +0300

    ☕ Expense serialization

--- lib/core/serializers/expense.dart
+++ lib/core/serializers/expense.dart
@@ -1,3 +1,23 @@
 import 'package:expense_manager/core/models/expense.dart';
 
-extension ExpenseSerializer on Expense {}
+extension ExpenseSerializer on Expense {
+  Map<String, dynamic> get serialized {
+    return {
+      'name': this.name,
+      'categoryID': this.categoryId,
+      'cost': this.cost,
+      'receiptData': this.receiptURL?.serialized,
+      'meta': this.meta.serialized,
+    };
+  }
+
+  static Expense deserialized(Map<String, dynamic> expenseObj) {
+    return Expense(
+      meta: expenseObj['meta'].toMeta,
+      name: expenseObj['name'],
+      categoryId: expenseObj['categoryId'],
+      cost: expenseObj['cost'],
+      receiptURL: expenseObj['receiptData'].toReceipt,
+    );
+  }
+}
