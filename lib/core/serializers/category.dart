009add446978e61358b469628aed1838bedd6ce6
Walter Banda <walterkaimurima@gmail.com>
2023-07-11 11:23:59 +0300

    ☕ Category serialization

--- lib/core/serializers/category.dart
+++ lib/core/serializers/category.dart
@@ -1,3 +1,19 @@
 import 'package:expense_manager/core/models/category.dart';
 
-extension CategorySerializer on ExpenseCategory {}
+extension CategorySerializer on ExpenseCategory {
+  Map<String, dynamic> get serialized {
+    return {
+      'name': this.name,
+      'budget': this.budget,
+      'meta': this.meta.serialized,
+    };
+  }
+
+  static ExpenseCategory deserialized(Map<String, dynamic> categoryObj) {
+    return ExpenseCategory(
+      meta: categoryObj['meta'].toMeta,
+      name: categoryObj['name'],
+      budget: categoryObj['budget'],
+    );
+  }
+}
