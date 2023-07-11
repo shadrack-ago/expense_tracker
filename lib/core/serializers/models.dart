19749a2816343373cce41b6d4cf51d69a2f8e4f5
Walter Banda <walterkaimurima@gmail.com>
2023-07-11 12:08:25 +0300

    ☕ Receipt Image serialization, deserialization

--- lib/core/serializers/models.dart
+++ lib/core/serializers/models.dart
@@ -1,5 +1,26 @@
 import 'package:expense_manager/core/models/expense.dart';
 
-extension RImageSerializer on ReceiptImage {}
+extension RImageSerializer on ReceiptImage {
+  Map<String, dynamic>? get serialized {
+    switch (this.type) {
+      case RImageType.file:
+        return {
+          'type': this.type,
+          'data': this.data.serialized,
+        };
+      case RImageType.network:
+        return {
+          'type': this.type,
+          'data': this.data.serialized,
+        };
+      case RImageType.memory:
+        return {
+          'type': this.type,
+          'data': this.data.serialized,
+        };
+      default:
+        return null;
+    }
+  }
 
 extension MetadataSerializer on MetaData {}
