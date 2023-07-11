ff7d2e7d3be0165c25a6fd9d7ce443b692c1edf2
Walter Banda <walterkaimurima@gmail.com>
2023-07-11 14:29:45 +0300

    ☕ Form state for category

--- lib/screens/add/category.dart
+++ lib/screens/add/category.dart
@@ -5,6 +5,48 @@ import 'package:provider/provider.dart';
 import '../../core/provider/manager.dart';
 import '../../router/index.dart';
 
+class _FormState {
+  _FormState();
+  final GlobalKey<FormState> key = GlobalKey();
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController budgetController = TextEditingController();
+
+  void submit({required DataManager callback, required BuildContext context}) {
+    if (key.currentState!.validate()) {
+      if (callback.getCategory(nameController.text.toLowerCase()) != null) {
+        Navigation.alert(
+          context: context,
+          builder: (_context) => AlertDialog(
+            title: Text('Category already exists'),
+            content: Text(
+                'The Category already exists please create a new one or edit the category'),
+            actions: [
+              TextButton.icon(
+                  icon: Icon(Icons.arrow_back_ios_new_rounded),
+                  onPressed: () => Navigator.pop(_context),
+                  style:
+                      TextButton.styleFrom(foregroundColor: Colors.blueAccent),
+                  label: Text('Create')),
+              TextButton.icon(
+                  icon: Icon(Icons.edit_document),
+                  onPressed: () {},
+                  label: Text('Edit')),
+            ],
+          ),
+        );
+      } else {
+        Provider.of<DataManager>(context, listen: false).addCategory(
+          CategoryForm(
+            name: nameController.text,
+            budget: double.parse(budgetController.text),
+          ),
+        );
+        Navigator.pop(context);
+      }
+    }
+  }
+}
+
 class AddCategory extends StatelessWidget {
   AddCategory({super.key});
 
