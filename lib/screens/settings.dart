ed445c4aa5989cc2daceda9e4f9e46b9a770988b
Walter Banda <walterkaimurima@gmail.com>
2023-07-12 18:55:13 +0300

    ☕ Built list for expenses and categories

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../core/provider/manager.dart';
 
 class Settings extends StatelessWidget {
   Settings({super.key});
@@ -9,7 +12,86 @@ class Settings extends StatelessWidget {
     return SingleChildScrollView(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: [],
+        children: [
+          Text(
+            'Edit properties',
+            style: Theme.of(context).textTheme.titleLarge,
+            textAlign: TextAlign.center,
+          ),
+          Card(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: AspectRatio(
+                aspectRatio: 16 / 20,
+                child: DefaultTabController(
+                  length: 2,
+                  child: Column(
+                    children: [
+                      TabBar(tabs: [
+                        Tooltip(
+                          preferBelow: false,
+                          message: 'Expenses',
+                          child: Tab(
+                            text: 'Expenses',
+                          ),
+                        ),
+                        Tooltip(
+                          preferBelow: false,
+                          message: 'Categories',
+                          child: Tab(
+                            text: 'Categories',
+                          ),
+                        ),
+                      ]),
+                      SizedBox(height: 12),
+                      AspectRatio(
+                        aspectRatio: 16 / 4,
+                        child: Consumer<DataManager>(
+                          builder: (context, instance, child) {
+                            return TabBarView(children: [
+                              ListView.separated(
+                                physics: BouncingScrollPhysics(),
+                                itemCount: instance.expenses.length,
+                                separatorBuilder: (context, index) => SizedBox(
+                                  height: 10,
+                                ),
+                                itemBuilder: (context, index) {
+                                  return Card(
+                                    child: ListTile(
+                                      title:
+                                          Text(instance.expenses[index].name),
+                                      trailing: Text(''),
+                                    ),
+                                  );
+                                },
+                              ),
+                              ListView.separated(
+                                physics: BouncingScrollPhysics(),
+                                itemCount: instance.categories.length,
+                                separatorBuilder: (context, index) => SizedBox(
+                                  height: 10,
+                                ),
+                                itemBuilder: (context, index) {
+                                  return Card(
+                                    child: ListTile(
+                                      title:
+                                          Text(instance.categories[index].name),
+                                      trailing: Text(''),
+                                    ),
+                                  );
+                                },
+                              )
+                            ]);
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
