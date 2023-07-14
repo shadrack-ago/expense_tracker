0119913e2da1e455e275b0ff1dc9318052dd4718
Walter Banda <walterkaimurima@gmail.com>
2023-07-14 15:05:35 +0300

    ☕ Map deserializations

--- lib/core/serializers/utils.dart
+++ lib/core/serializers/utils.dart
@@ -1,1 +1,24 @@
-extension Deserialization on Map<String, dynamic> {}
+import 'package:expense_manager/core/models/category.dart';
+import 'package:expense_manager/core/serializers/category.dart';
+import 'package:expense_manager/core/serializers/expense.dart';
+
+import '../models/expense.dart';
+import 'models.dart';
+
+extension Deserialization on Map<String, dynamic> {
+  MetaData get toMeta {
+    return MetadataSerializer.deserialized(this);
+  }
+
+  static ExpenseCategory toCategory(Map<String, dynamic> data) {
+    return CategorySerializer.deserialized(data);
+  }
+
+  Expense get toExpense {
+    return ExpenseSerializer.deserialized(this);
+  }
+
+  ReceiptImage? get toImage {
+    return RImageSerializer.deserialized(this);
+  }
+}
